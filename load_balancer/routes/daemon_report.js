4dce8d86f63d101d42699f349ac5629b3f2d1f35
rmxhaha <code000123@gmail.com>
2017-04-23 21:09:46 +0700

    error handling rogue input

--- load_balancer/routes/daemon_report.js
+++ load_balancer/routes/daemon_report.js
@@ -5,7 +5,18 @@ let config = require('../config.json');
 /* GET home page. */
 router.get('/:worker_id/', function(req, res, next) {
   const worker_id = req.params.worker_id;
-  const cpuload = req.query.cpuload;
+  let cpuload = req.query.cpuload;
+
+  if( cpuload > 100 )
+    cpuload = 100;
+  else if( cpuload < 0 )
+    cpuload = 0;
+  else {
+    res.status(422).json({
+      'status' : 'fail',
+      'message' : 'cpuload not given'
+    });
+  }
 
   console.log( "Worker #" + worker_id + ": reporting CPU " + cpuload + "%" );
 
