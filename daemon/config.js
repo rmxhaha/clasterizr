d9be55b21ec8f9ed22ca9090185227647e8921ed
rmxhaha <code000123@gmail.com>
2017-04-28 22:43:40 +0700

    workerAddress in env; cpuusage get using os-utils

--- daemon/config.js
+++ daemon/config.js
@@ -6,8 +6,7 @@ module.exports = {
     "http://localhost:47004",
     "http://localhost:47005"
   ],
-  "storage_location" : __dirname + '/data/node_state' + process.env.NODEID + '.json',
-  "log_location" : __dirname + '/data/node_log' + process.env.NODEID + '.txt',
-  'send_interval' : 200,
-  'daemonId' : process.env.DAEMONID
+  'send_interval' : 2000,
+  'daemonId' : process.env.DAEMONID,
+  'workerAddr' : process.env.WORKERADDR
 }
--- daemon/index.js
+++ daemon/index.js
@@ -3,24 +3,30 @@
 let rp = require('request-promise')
 let config = require('./config.js')
 let Promise = require('bluebird')
+let osu = require('os-utils')
 
 setInterval(()=>{
-  let arr = []
-  for( let i = 0; i < config.node_addresses.length; ++ i )
-    arr.push(
-      rp({
-        method : "GET",
-        uri : config.node_addresses[i] + "/load_balancer/receiveNewLog/" + config.daemonId + "/?cpuload=10&address=http://localhost:13337"
-      })
-        .then((body)=>{
-          if( body.status == "fail" )
-            return Promise.reject()
-          return Promise.resolve()
+  osu.cpuUsage((cpuload)=>{
+    cpuload = Math.ceil( cpuload * 100)
+    console.log(cpuload)
+    let arr = []
+    for( let i = 0; i < config.node_addresses.length; ++ i )
+      arr.push(
+        rp({
+          method : "GET",
+          uri : config.node_addresses[i] + "/load_balancer/receiveNewLog/" + config.daemonId + "/?cpuload="+cpuload+"&address=" + config.workerAddr
         })
-    )
+          .then((body)=>{
+            if( body.status == "fail" )
+              return Promise.reject()
+            return Promise.resolve()
+          })
+      )
 
-  Promise.any(arr)
-    .then(()=>console.log("send ok"))
-    .catch(()=>console.log("cannot contact load balancer"))
+    Promise.any(arr)
+      .then(()=>console.log("send ok"))
+      .catch(()=>console.log("cannot contact load balancer"))
+
+  })
 
 }, config.send_interval)
